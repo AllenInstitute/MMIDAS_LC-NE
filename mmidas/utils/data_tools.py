66f18d5f1688e528694a5ea90be2b9b5a7661ce0
Yeganeh <ymmarghi@gmail.com>
2025-01-31 13:44:02 -0800

    Add - new dataloader for dbh-retroseq data

--- mmidas/utils/data_tools.py
+++ mmidas/utils/data_tools.py
@@ -1,16 +1,17 @@
 import numpy as np
-import scipy.sparse as ss
 import scanpy as sc
 import seaborn as sns
 from sklearn.preprocessing import normalize
 import matplotlib.colors as mcolors
 from sklearn.feature_extraction.text import TfidfTransformer
 import pandas as pd
+import pdb
 from scipy.sparse import issparse
-import matplotlib.pyplot as plt
 import torch
 from torch.utils.data import DataLoader, TensorDataset
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
 
 
 
@@ -176,13 +177,14 @@ def load_data(file, gene_file='', n_gene=0):
             print("--------> Cannot combine dataset! <--------")
     else:
         adata = sc.read_h5ad(file)
+        genes = adata.var.index.values
         if gene_file:
             g_index = [np.where(genes == g)[0][0] for g in gene_list if g in genes]
             data['log1p'] = adata.X[:, g_index].toarray()
             data['gene_id'] = gene_list
         else:
             data['log1p'] = adata.X.toarray()
-            data['gene_id'] = adata.var.index.values
+            data['gene_id'] = np.hstack(adata.var.values)
     
     if n_gene > 0:
         data['log1p'] = data['log1p'][:, :n_gene]
@@ -215,7 +217,7 @@ def get_data(x, train_size, additional_val, seed):
 
 
 
-def get_loaders(x, label=[], batch_size=128, train_size=0.9, n_aug_smp=0, netA=None, aug_param=0., device=None, additional_val=False, seed=0):
+def get_loaders(x, label=[], batch_size=128, train_size=0.9, n_aug_smp=0, netA=None, additional_val=False, seed=0):
 
     if len(label) > 0:
         train_ind, val_ind, test_ind = [], [], []
@@ -248,14 +250,7 @@ def get_loaders(x, label=[], batch_size=128, train_size=0.9, n_aug_smp=0, netA=N
         train_set_ind = train_ind_torch.clone()
         for _ in range(n_aug_smp):
             if netA:
-                noise = 0.1*torch.randn(train_set_torch.shape[0], aug_param['num_n'], device=device)
-                if device:
-                    _, gen_data = netA(train_set_torch.cuda(device), noise, True, device)
-                else:
-                    _, gen_data = netA(train_set_torch, noise, True, device)
-                data_bin = 0. * train_set_torch
-                data_bin[train_set_torch > 1e-4] = 1.
-                fake_data = gen_data * data_bin
+                _, fake_data = netA(train_set, True, .1)
                 train_set = torch.cat((train_set, fake_data.cpu().detach()), 0)
 
             else:
@@ -291,5 +286,150 @@ def get_loaders(x, label=[], batch_size=128, train_size=0.9, n_aug_smp=0, netA=N
     return alldata_loader, train_loader, test_loader, validation_loader, test_ind, val_ind
 
 
+def Dbh_Retro_loaders(x_Dbh, x_Retro, label, batch_size=128, train_size=0.9, n_aug_smp=0, netA=None, additional_val=False, seed=0):
 
+    tt_size = int(train_size * x_Dbh.shape[0])
+    train_set_Dbh, val_set_Dbh, test_set_Dbh, train_ind_Dbh, val_ind_Dbh, test_ind_Dbh = get_data(x_Dbh, tt_size, additional_val, seed)
+
+    train_set_torch_Dbh = torch.FloatTensor(train_set_Dbh)
+    train_ind_torch_Dbh = torch.FloatTensor(train_ind_Dbh)
+    if n_aug_smp > 0:
+        train_set_Dbh = train_set_torch_Dbh.clone()
+        train_set_ind_Dbh = train_ind_torch_Dbh.clone()
+        for _ in range(n_aug_smp):
+            if netA:
+                _, fake_data = netA(train_set_Dbh, True, .1)
+                train_set_Dbh = torch.cat((train_set_Dbh, fake_data.cpu().detach()), 0)
+
+            else:
+                train_set_Dbh = torch.cat((train_set_Dbh, train_set_torch_Dbh), 0)
+                
+            train_set_ind_Dbh = torch.cat((train_set_ind_Dbh, train_ind_torch_Dbh), 0)
+        
+        train_set_torch_Dbh = train_set_Dbh.clone()
+        train_ind_torch_Dbh = train_set_ind_Dbh.clone()
+        print(f'New size of Dbh: {train_set_torch_Dbh.shape[0]}')
+    
+    if len(label) > 0:
+        train_ind, val_ind, test_ind = [], [], []
+        for ll in np.unique(label):
+            indx = np.where(label == ll)[0]
+            tt_size = int(train_size * sum(label == ll))
+            _, _, _, train_subind, val_subind, test_subind = get_data(x_Retro[indx], tt_size, additional_val, seed)
+            train_ind.append(indx[train_subind])
+            test_ind.append(indx[test_subind])
+            if additional_val:
+                val_ind.append(indx[val_subind])
+
+        train_ind_retro = np.concatenate(train_ind)
+        test_ind_retro = np.concatenate(test_ind)
+        train_set_retro = x_Retro[train_ind_retro]
+        test_set_retro = x_Retro[test_ind_retro]
+        
+        if additional_val:
+            val_ind_retro = np.concatenate(val_ind)
+            val_set_retro = x_Retro[val_ind_retro]
+        
+    else:
+        tt_size = int(train_size * x_Retro.shape[0])
+        train_set_retro = x_Retro.copy()
+        train_ind_retro = range(x_Retro.shape[0])
+        val_set_retro = x_Retro.copy()
+        val_ind_retro = range(x_Retro.shape[0])
+        test_set_retro = x_Retro.copy()
+        test_ind_retro = range(x_Retro.shape[0])
+
+    train_set_torch_retro = torch.FloatTensor(train_set_retro)
+    train_ind_torch_retro = torch.FloatTensor(train_ind_retro)
+    if n_aug_smp > 0:
+        train_set_retro = train_set_torch_retro.clone()
+        train_set_ind_retro = train_ind_torch_retro.clone()
+        n_aug_smp_ = n_aug_smp * int(x_Dbh.shape[0]/ x_Retro.shape[0])
+        print(f'Number of augmentation samples for RetroSeq: {n_aug_smp_}')
+        for _ in range(n_aug_smp_):
+            if netA:
+                _, fake_data = netA(train_set_retro, True, .1)
+                train_set_retro = torch.cat((train_set_retro, fake_data.cpu().detach()), 0)
+            else:
+                train_set_retro = torch.cat((train_set_retro, train_set_torch_retro), 0)
+                
+            train_set_ind_retro = torch.cat((train_set_ind_retro, train_ind_torch_retro), 0)
+        
+        train_set_torch_retro = train_set_retro.clone()
+        train_ind_torch_retro = train_set_ind_retro.clone()
+        print(f'New size of retro: {train_set_torch_retro.shape[0]}')
+        
+    train_data = TensorDataset(torch.cat((train_set_torch_Dbh, train_set_torch_retro), dim=0), torch.cat((train_ind_torch_Dbh, train_ind_torch_retro), dim=0))
+    train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True, drop_last=True, pin_memory=True)
+
+    test_set_torch = torch.FloatTensor(np.concatenate((test_set_Dbh, test_set_retro), axis=0))
+    test_ind_torch = torch.FloatTensor(np.concatenate((test_ind_Dbh, test_ind_retro), axis=0))
+    test_data = TensorDataset(test_set_torch, test_ind_torch)
+    test_loader = DataLoader(test_data, batch_size=1, shuffle=True, drop_last=False, pin_memory=True)
+
+    data_set_troch = torch.FloatTensor(np.concatenate((x_Dbh, x_Retro), axis=0))
+    all_ind_torch = torch.FloatTensor(range(x_Dbh.shape[0] + x_Retro.shape[0]))
+    all_data = TensorDataset(data_set_troch, all_ind_torch)
+    alldata_loader = DataLoader(all_data, batch_size=batch_size, shuffle=False, drop_last=False, pin_memory=True)
+    
+    if additional_val:
+        val_set_torch = torch.FloatTensor(np.concatenate((val_set_Dbh, val_set_retro), axis=0))
+        val_ind_torch = torch.FloatTensor(np.concatenate((val_ind_Dbh, val_ind_retro), axis=0))
+        validation_data = TensorDataset(val_set_torch, val_ind_torch)
+        validation_loader = DataLoader(validation_data, batch_size=batch_size, shuffle=True, drop_last=False, pin_memory=True)
+    else:
+        validation_loader = []
+        val_ind = []
+        
+    return alldata_loader, train_loader, test_loader, validation_loader, test_ind, val_ind
+
+
+
+def select_best_genes(X, y, n_top_genes=500, K_fold=10, n_repeat=10):
+    
+    mutual_features = []
+    for repeat in range(n_repeat):
+        train_ind, test_ind = split_data_Kfold(y, K_fold)
+        collect_features = []
+        for fold in range(K_fold):
+            print(f"------- {repeat} - {fold} -------")
+            train_id = train_ind[fold].astype(int)
+            test_id = test_ind[fold].astype(int)
+            X_train, X_test = X[train_id, :], X[test_id, :]
+            y_train, y_test = y[train_id], y[test_id]
+            clf = RandomForestClassifier()
+            clf.fit(X_train, y_train)
+            y_pred = clf.predict(X_test)
+            accuracy_all = accuracy_score(y_test, y_pred)
+
+            # Get feature importance scores
+            feature_importances = clf.feature_importances_
+            # Select top `n_top_genes`
+            top_gene_indices = np.argsort(feature_importances)[::-1][:n_top_genes]
+            collect_features.append(top_gene_indices)
+
+            # Train and evaluate with selected genes
+            X_train_sel, X_test_sel = X_train[:, top_gene_indices], X_test[:, top_gene_indices]
+            clf = RandomForestClassifier()
+            clf.fit(X_train_sel, y_train)
+            y_pred = clf.predict(X_test_sel)
+            accuracy_select = accuracy_score(y_test, y_pred)
+            print(f"Accuracy before feature selection: {accuracy_all}")
+            print(f"Accuracy after feature selection: {accuracy_select}")
+        
+        mutual_features.append(np.array(list(set.intersection(*(set(ff) for ff in collect_features)))))
+        clf = RandomForestClassifier()
+        clf.fit(X[:, mutual_features[-1]], y)
+        y_pred = clf.predict(X[:, mutual_features[-1]])
+        accuracy_select = accuracy_score(y, y_pred)
+        print(f"Overall accuracy after feature selection: {accuracy_select}, for {len(mutual_features[-1])} genes")
+    
+    final_features = np.array(list(set.union(*(set(ff) for ff in mutual_features))))
+    clf = RandomForestClassifier()
+    clf.fit(X[:, final_features], y)
+    y_pred = clf.predict(X[:, final_features])
+    accuracy_select = accuracy_score(y, y_pred)
+    print(f"Overall accuracy after feature selection: {accuracy_select}, for {len(final_features)} genes")
+    
+    return final_features
 
